93cd456694d28a10aa8bca03926c181c8924e55b
Gamba <juanca.quinterom@gmail.com>
2025-01-31 19:19:36 -0500

    Bear color material

--- Assets/_Game/Scripts/Gameplay/Bear/BearController.cs
+++ Assets/_Game/Scripts/Gameplay/Bear/BearController.cs
@@ -2,8 +2,32 @@ using UnityEngine;
 
 public class BearController : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField]
+    private Material colorMaterial;
+
+    #region Public Methods
+
     public void Play(Color color)
     {
-
+        colorMaterial.color = color;
     }
+
+    #endregion
+
+    // ----------------------------------------------------------------------------------------------------------------------------
+
+    #region Editor
+
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        colorMaterial.color = Color.white;
+    }
+
+#endif
+
+    #endregion
+
 }
