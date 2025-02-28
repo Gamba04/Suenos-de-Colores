c213526e945f53bc57c97d6f3d6fab65fcb17009
Gamba <juanca.quinterom@gmail.com>
2025-02-27 23:50:32 -0500

    WalkingArea Inspector polish

--- Assets/_Game/Scripts/Gameplay/Walking Bear/WalkingBearsController.cs
+++ Assets/_Game/Scripts/Gameplay/Walking Bear/WalkingBearsController.cs
@@ -29,7 +29,122 @@ public class WalkingBearsController : MonoBehaviour
         }
     }
 
-#endregion
+    #region Custom Editor
+
+#if UNITY_EDITOR
+
+    [CustomPropertyDrawer(typeof(WalkingArea))]
+    private class WalkingAreaDrawer : PropertyDrawer
+    {
+
+        #region OnGUI
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            position.height = EditorGUIUtility.singleLineHeight;
+
+            DrawMainProperty(position, property, label);
+            DrawChildProperties(position, property.Copy());
+        }
+
+        private void DrawMainProperty(Rect position, SerializedProperty property, GUIContent label)
+        {
+            property.isExpanded = EditorGUI.Foldout(position, property.isExpanded, label);
+        }
+
+        private void DrawChildProperties(Rect position, SerializedProperty property)
+        {
+            if (property.isExpanded)
+            {
+                if (property.NextVisible(true))
+                {
+                    do
+                    {
+                        position.y += EditorGUI.GetPropertyHeight(property);
+
+                        if (property.type == nameof(Vector3)) DrawXZVector(position, property);
+                        else EditorGUI.PropertyField(position, property);
+
+                        position.y += EditorGUIUtility.standardVerticalSpacing;
+                    }
+                    while (property.NextVisible(false));
+                }
+            }
+        }
+
+        private void DrawXZVector(Rect position, SerializedProperty property)
+        {
+            DrawLabel();
+            DrawComponents();
+
+            void DrawLabel()
+            {
+                EditorGUI.indentLevel++;
+
+                position = EditorGUI.PrefixLabel(position, EditorGUIUtility.TrTempContent(property.displayName));
+
+                EditorGUI.indentLevel--;
+            }
+
+            void DrawComponents()
+            {
+                EditorGUIUtility.labelWidth = 10;
+
+                int amount = 2;
+                float spacing = 3.5f;
+
+                position.width = (position.width - spacing * (amount - 1)) / amount;
+
+                DrawComponent(nameof(Vector3.x));
+                DrawComponent(nameof(Vector3.z));
+
+                EditorGUIUtility.labelWidth = 0;
+
+                void DrawComponent(string component)
+                {
+                    EditorGUI.PropertyField(position, property.FindPropertyRelative(component));
+
+                    position.x += position.width + spacing;
+                }
+            }
+        }
+
+        #endregion
+
+        // ----------------------------------------------------------------------------------------------------------------------------
+
+        #region GetPropertyHeight
+
+        public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+        {
+            float height = EditorGUIUtility.singleLineHeight;
+
+            if (property.isExpanded)
+            {
+                property = property.Copy();
+
+                if (property.NextVisible(true))
+                {
+                    do
+                    {
+                        height += EditorGUI.GetPropertyHeight(property) + EditorGUIUtility.standardVerticalSpacing;
+                    }
+                    while (property.NextVisible(false));
+                }
+            }
+
+            return height;
+        }
+
+        #endregion
+
+    }
+
+#endif
+
+    #endregion
+
+    #endregion
 
     [Header("Components")]
     [SerializeField]
